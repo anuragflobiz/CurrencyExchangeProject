4fe08ada71a0eedc6748f6f77b858df5204a7edf
Anurag Pandey <anurag.ppandey@flobiz.in>
2025-12-23 23:07:06 +0530

    Initial commit

---
+++ src/test/java/com/CurrencyExchange/CurrencyExchangeProject/CurrencyExchangeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CurrencyExchange.CurrencyExchangeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
