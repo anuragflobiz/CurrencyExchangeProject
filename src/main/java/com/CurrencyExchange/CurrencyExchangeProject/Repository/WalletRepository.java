c9b14c23ae87cf853cf153f6d21db664ecf9ea39
Anurag Pandey <anurag.ppandey@flobiz.in>
2025-12-24 14:36:02 +0530

    fix imports

--- src/main/java/com/CurrencyExchange/CurrencyExchangeProject/Repository/WalletRepository.java
+++ src/main/java/com/CurrencyExchange/CurrencyExchangeProject/Repository/WalletRepository.java
@@ -1,6 +1,8 @@
 package com.CurrencyExchange.CurrencyExchangeProject.Repository;
 
-
+import com.CurrencyExchange.CurrencyExchangeProject.Entity.User;
+import com.CurrencyExchange.CurrencyExchangeProject.Entity.Wallet;
+import com.CurrencyExchange.CurrencyExchangeProject.enums.CurrencyCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
