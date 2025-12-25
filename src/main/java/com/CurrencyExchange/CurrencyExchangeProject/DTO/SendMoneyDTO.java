d5ed88a03ad8c26b1d6bd8ff7b2d40fc45c1d4a9
Anurag Pandey <anurag.ppandey@flobiz.in>
2025-12-25 16:53:06 +0530

    Added validation at DTO

--- src/main/java/com/CurrencyExchange/CurrencyExchangeProject/DTO/SendMoneyDTO.java
+++ src/main/java/com/CurrencyExchange/CurrencyExchangeProject/DTO/SendMoneyDTO.java
@@ -1,5 +1,7 @@
 package com.CurrencyExchange.CurrencyExchangeProject.DTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,13 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SendMoneyDTO {
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be greater than 0")
     private BigDecimal senderAmount;
+
+    @NotNull(message = "From wallet id is required")
     private UUID fromWalletId;
-    private UUID toWalletId;;
+
+    @NotNull(message = "To wallet id is required")
+    private UUID toWalletId;
 }
