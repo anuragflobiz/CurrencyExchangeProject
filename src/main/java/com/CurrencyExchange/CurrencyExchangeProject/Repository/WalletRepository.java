27115131a2050014578b68ccd53592e5cfa9a8bf
Anurag Pandey <anurag.ppandey@flobiz.in>
2025-12-30 15:26:46 +0530

    added lock on db row

--- src/main/java/com/CurrencyExchange/CurrencyExchangeProject/Repository/WalletRepository.java
+++ src/main/java/com/CurrencyExchange/CurrencyExchangeProject/Repository/WalletRepository.java
@@ -3,7 +3,9 @@ package com.CurrencyExchange.CurrencyExchangeProject.Repository;
 import com.CurrencyExchange.CurrencyExchangeProject.Entity.User;
 import com.CurrencyExchange.CurrencyExchangeProject.Entity.Wallet;
 import com.CurrencyExchange.CurrencyExchangeProject.enums.CurrencyCode;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -20,4 +22,12 @@ public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 
     Optional<Wallet> findByUserIdAndCurrencyCode(UUID id, CurrencyCode currencyCode);
     List<Wallet> findAllByUserId(UUID id);
+
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        select w from Wallet w
+        where w.id = :senderId or w.id = :recieverId
+    """)
+    List<Wallet> findSendRecieverWallet(UUID senderId, UUID recieverId);
 }
