8b94acc9bf11690efd6aa41309b8c96bbe2fe25d
Anurag Pandey <anurag.ppandey@flobiz.in>
2026-01-11 11:21:59 +0530

    remove extra import in rep

--- src/main/java/com/CurrencyExchange/CurrencyExchangeProject/Repository/WalletRepository.java
+++ src/main/java/com/CurrencyExchange/CurrencyExchangeProject/Repository/WalletRepository.java
@@ -1,12 +1,8 @@
 package com.CurrencyExchange.CurrencyExchangeProject.Repository;
 
-import com.CurrencyExchange.CurrencyExchangeProject.Entity.User;
 import com.CurrencyExchange.CurrencyExchangeProject.Entity.Wallet;
 import com.CurrencyExchange.CurrencyExchangeProject.Enums.CurrencyCode;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
